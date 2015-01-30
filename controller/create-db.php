a6a12f8788889920e91cd07962319ef6a3b9575b
Junior Ramirez <Juniorr87@yahoo.com>
2015-01-30 13:58:32 -0800

    1/30/15 blog

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,8 +6,19 @@ $connection = new mysqli($host, $username, $password);
 if($connection->connect_error){
     die("Error: " . $connection->connect_error);
 }
-else{
-    echo "Success: " . $connection->host_info;
+
+
+$exists = $connection->select_db($database);
+
+
+if(!$exists){
+    $query = $connection->query("CREATE DATABASE $database ");
+    if($query){
+        echo "Successfully created database: " . $database;
+    }
+}
+else {
+    echo "Database already exists";
 }
 
 $connection->close();
