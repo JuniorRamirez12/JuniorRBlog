25f2d7dc5b134d4cba97cf6f2d2996877520aafe
Junior Ramirez <Juniorr87@yahoo.com>
2015-02-23 14:18:09 -0800

    blog 2/23/15

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,26 +1,7 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");
 
-//$connection = new mysqli($host, $username, $password);
-
-//if($connection->connect_error){
-    die("<p>Error: " . $connection->connect_error . "</p>");
-//}
-
-//$exists = $connection->select_db($database);
-
-//if(!$exists){
-    //$query = $connection->query("CREATE DATABASE $database ");
-    
-    //if($query){
-       // echo "Successfully created database: " . $database;
-   // }
-//}
-//else {
-    //echo "Database already exists";
-//}
-
-$query = $connection->query("CREATE TABLE posts ("
+$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
         . "title varchar(255) NOT NULL,"
         . "post text NOT NULL,"
@@ -30,7 +11,20 @@ if($query){
     echo"<p>Successfully created table: posts</p>";
 }
 else {
-    echo "<p>$connection->error</p>";
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
 
-//$connection->close();
+$query = $_SESSION["connection"]->query("CREATE TABLE users("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query){
+    echo"<p>Successfully created table: users</p>";
+}
+else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
--- model/Database.php
+++ model/Database.php
@@ -13,27 +13,23 @@ class Database {
        $this->username = $username;
        $this->password = $password;
        $this->database = $database;
-    
+   }
+   
+   
+   
+    public function openConnection(){
         $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+    }
+       
+      
         
-        if ($connection->connect_error) {
-            die("<p>Error: " . $connection->connect_error . "</p>");
-        }
-        
-        $exists = $this->connection->select_db($database);
-        
-        if (!$exists) {
-            $query = $this->connection->query("CREATE DATABASE $database");
-           
-           if($query) {
-               echo "<p>Successfully created database: " . $database . "</p>";
-           }
     public function closeConnection(){
     if(isset($this->connection)) {
-            $this->connection->close();        }
-    }
-    
-    public function query($string){
+            $this->connection->close();  
+           }
+        }
+   
+    public function query($string) {
         
+        }
     }
-}
