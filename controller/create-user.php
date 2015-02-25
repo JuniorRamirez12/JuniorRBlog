abb7f04b5f01ec80078172d9926d38be5daf95fb
Junior Ramirez <Juniorr87@yahoo.com>
2015-02-25 13:58:25 -0800

    2/25/15 blog

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,5 +5,24 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
     
-    echo $email . " - " . $username . " - " . $password;
+    echo $password;
+    //echo $email . " - " . $username . " - " . $password;
     
+    $salt = "$5$" . "rounds=5000" . uniqid(nt_rand(), true) . "$";
+    
+    //echo $salt;
+    $hashedPassword = crypt($password, $salt);
+    
+    //echo $hashedPassword;
+    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+            . "email = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedPassword',"
+            . "salt = '$salt'");
+    
+    if($query) {
+        echo "Successfully created user: $username";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
